4e72e78e3bc620a9e9e5ac21ee54961fa1c1542d
Katy <katyrocks1928@gmail.com>
2015-12-12 21:08:19 +0000

    adding finishing files, finished

--- script.js
+++ script.js
@@ -1,29 +1,47 @@
 $(document).ready(function(){
     var status="waiting";
     var score= 0;
+    var ping = new Audio();
+    ping.src = "music.mp3";
+    var fail = new Audio();
+    fail.src = "fail.mp3";
+    var tada = new Audio();
+    tada.src = "tada.mp3";
+
+
     $('.planet').mouseenter( function(){
         if (status == "started") {
-           $('#score').text("GAME OVER! Your score is " + score);
+            fail.play();
+//           $('#score').text("GAME OVER! Score: " + score);
             status = "lost";
+            $('#button').show();
+            $.featherlight("lose.html");
+    
         }
     });
           
+
     document.clickStart = function(){
         if (status != "started") {
             status = "started";
             score = 0;
             resize();
+
+            ping.play();
         
-            $('#score').text(score);
+            $('#score').text("Score: " + score);
             $('.collect').show();
+            $('#button').hide();
         } else {
              alert("You have already started");
         }
-    }       
+    };       
 
 function resize() {
-    $('.planet').animate({width:'-=20px',height:'-=20px'},"slow");
-    $('.planet').animate({width:'+=20px',height:'+=20px'},"slow",resize);
+    if (status == "started") {
+        $('.planet').animate({width:'-=20px',height:'-=20px'},"slow");
+        $('.planet').animate({width:'+=20px',height:'+=20px'},"slow",resize);
+    }
 }
          
     
@@ -32,13 +50,21 @@ function resize() {
      $('.collect').mouseenter(function(event){
         if (status == "started"){
             score = score + 1;
-            $('#score').text(score); 
+            $('#score').text("Score: " + score); 
             $(event.target).hide();
         }
     });
     
     $('#earth').mouseenter(function(){
-        $('#score').text("YOU HAVE WON!! Your score is " + score);
+        if (status == "started") {
+        tada.play();
+//        $('#score').text("YOU HAVE WON!! Your score is " + score);
+        status = "won";
+        $.featherlight("win.html");
+        }
+        $('#button').show();
     });
+    
+    
     
 });
